fc9332af8683462a2784e403f4e1d5acac813250
uael <lucas.abel@yandex.com>
2017-09-21 01:10:12 +0200

    oops

--- test/uopt.c
+++ test/uopt.c
@@ -180,6 +180,5 @@ CUTEST(opt, duplicate) {
   ASSERT(memcmp(err.msg, STRNSIZE("duplicate value for command line option ‘o’: ‘bla’")) == 0);
   err = self->opts.errs.buf[4];
   ASSERT(memcmp(err.msg, STRNSIZE("duplicate command line option ‘S’")) == 0);
-  err_stack_dump(&self->opts.errs, stdout);
   return CUTE_SUCCESS;
 }
